ce8b8f0c6d5edf6d771a20eb885c5d87e71e2361
Carlos Rivadeneira <carlosrivadeneiraz@gmail.com>
2015-12-02 13:05:05 -0500

    Marcado final + sprites del modulo 4 - tema 1

--- modulos/config.rb
+++ modulos/config.rb
@@ -3,16 +3,16 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "../css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "../img"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
