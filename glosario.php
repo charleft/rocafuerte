cfa5912d459f9b0909f9171fc737e4b4662c8bc0
Carlos Rivadeneira <carlosrivadeneiraz@gmail.com>
2015-12-02 06:58:54 -0500

    Add files from last state before start

---
+++ glosario.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Glosario</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link rel="stylesheet" href="css/styleGlosario.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="shortcut icon" type="image/x-icon" href="http://rocafuerte.com/templates/joomlage0071-simplex/favicon.ico">
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+</head>
+<body>
+	<div class="navbar-fixed"> 
+		<nav class="blue darken-4">
+			<div class="nav-wrapper container">
+				<a href="#"data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+				<ul class=" hide-on-med-and-down">
+					<li><a href="index.html#Inicio" class="page-scroll">INICIO</a></li>
+					<li><a href="index.html#Nosotros" class="page-scroll">INTRODUCCI&Oacute;N</a></li>
+					<li><a href="index.html#Objetivos" class="page-scroll">OBJETIVOS</a></li>
+					<li><a href="index.html#Modulos" class="page-scroll">M&Oacute;DULOS</a></li>
+					<li><a href="index.html#Simulador" class="page-scroll">CALCULADORA</a></li>
+					<li><a href="index.html#Contacto" class="page-scroll">CONT&Aacute;CTENOS</a></li>
+				</ul>
+				<ul class="side-nav" id="mobile-demo">
+					<li><a href="index.html#Inicio" class="page-scroll">INICIO</a></li>
+					<li><a href="index.html#Nosotros" class="page-scroll">INTRODUCCI&Oacute;N</a></li>
+					<li><a href="index.html#Objetivos" class="page-scroll">OBJETIVOS</a></li>
+					<li><a href="index.html#Modulos" class="page-scroll">M&Oacute;DULOS</a></li>
+					<li><a href="index.html#Simulador" class="page-scroll">CALCULADORA</a></li>
+					<li><a href="index.html#Contacto" class="page-scroll">CONT&Aacute;CTENOS</a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<header id="Hero">
+		<div class="malla"></div>
+		<h1 class="white-text center-align">glosario</h1>	
+	</header>
+		<form action="" method="post">
+			<nav class="orange">
+			<div class="nav-wrapper">
+				<div class="container">
+					<a class="btn modal-trigger hide-on-large-only orange" href="#modal1"><i class="material-icons right">list</i></a>
+				</div>
+				<ul class="hide-on-med-and-down ulABC">
+					
+					<li><input type="submit" name="A" value ="A" class="btnList"></li>
+					<li><input type="submit" name="B" value ="B" class="btnList"></li>
+					<li><input type="submit" name="C" value ="C" class="btnList"></li>
+					<li><input type="submit" name="D" value ="D" class="btnList"></li>
+					<li><input type="submit" name="E" value ="E" class="btnList"></li>
+					<li><input type="submit" name="F" value ="F" class="btnList"></li>
+					<li><input type="submit" name="G" value ="G" class="btnList"></li>
+					<li><input type="submit" name="H" value ="H" class="btnList"></li>
+					<li><input type="submit" name="I" value ="I" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="J" value ="J" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="K" value ="K" class="btnList"></li>
+					<li><input type="submit" name="L" value ="L" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="M" value ="M" class="btnList"></li>
+					<li><input type="submit" name="N" value ="N" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="Ñ" value ="Ñ" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="O" value ="O" class="btnList"></li>
+					<li><input type="submit" name="P" value ="P" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="Q" value ="Q" class="btnList"></li>
+					<li><input type="submit" name="R" value ="R" class="btnList"></li>
+					<li><input type="submit" name="S" value ="S" class="btnList"></li>
+					<li><input type="submit" name="T" value ="T" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="U" value ="U" class="btnList"></li>
+					<li><input type="submit" name="V" value ="V" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="W" value ="W" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="X" value ="X" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="Y" value ="Y" class="btnList"></li>
+					<li class="disabled"><input type="submit" name="Z" value ="Z" class="btnList"></li>
+				</ul>
+			</div>
+		</nav>
+		<div id="modal1" class="modal bottom-sheet">
+		  	<div class="modal-content center">
+				<input type="submit" name="A" value ="A" class="key modal-close">
+				<input type="submit" name="B" value ="B" class="key modal-close">
+				<input type="submit" name="C" value ="C" class="key modal-close">
+				<input type="submit" name="D" value ="D" class="key modal-close">
+				<input type="submit" name="E" value ="E" class="key modal-close">
+				<input type="submit" name="F" value ="F" class="key modal-close">
+				<input type="submit" name="G" value ="G" class="key modal-close">
+				<input type="submit" name="H" value ="H" class="key modal-close">
+				<input type="submit" name="I" value ="I" class="key modal-close">
+				<input type="submit" name="J" value ="J" class="disabled key">
+				<input type="submit" name="K" value ="K" class="disabled key">
+				<input type="submit" name="L" value ="L" class="key modal-close">
+				<input type="submit" name="M" value ="M" class="disabled key">
+				<input type="submit" name="N" value ="N" class="key modal-close">
+				<input type="submit" name="Ñ" value ="Ñ" class="disabled key">
+				<input type="submit" name="O" value ="O" class="disabled key">
+				<input type="submit" name="P" value ="P" class="key modal-close">
+				<input type="submit" name="Q" value ="Q" class="disabled key">
+				<input type="submit" name="R" value ="R" class="key modal-close">
+				<input type="submit" name="S" value ="S" class="key modal-close">
+				<input type="submit" name="T" value ="T" class="key modal-close">
+				<input type="submit" name="U" value ="U" class="disabled key">
+				<input type="submit" name="V" value ="V" class="key modal-close">
+				<input type="submit" name="W" value ="W" class="disabled key">
+				<input type="submit" name="X" value ="X" class="disabled key">
+				<input type="submit" name="Y" value ="Y" class="disabled key">
+				<input type="submit" name="Z" value ="Z" class="disabled key">
+
+			</div>
+		</div>
+	</form>
+	<div id="Contenidos" class="container">
+		<!-- <div class="row"> -->
+					<!-- </div> -->
+	</div>
+	<!-- <div id="Simbolo"> <img src="img/simbolosFooter.png" class="simbolo responsive-img hide-on-med-and-down" alt=""></div> -->
+	<!--<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>-->
+	<!--<script src="ejecutar.js"></script>-->
+	<script src="js/int.js"></script>
+	<script src="js/materialize.min.js" ></script>
+</body>
+</html>
---
+++ js/int_2.js
@@ -0,0 +1,246 @@
+// ******** Scroll ********//
+$(document).ready(function() {
+ 	$(".button-collapse").sideNav();
+ 	$('.parallax').parallax();
+ 	$('.slider').slider({full_width: true});
+ 	$('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    $('.modal-trigger').leanModal();
+});
+
+// ******** Paginador ********//
+//Guardar el contenedor principal
+var c = $('#Slider');
+// c.wrapInner('<div class="slider-margin" />');
+//Guardar los contenidos del Slider
+var s = c.find('.contenido');
+//Guardar el número de contenidos
+var n = s.length;
+//Guardar el "slider-inner"
+var ci = $('.slider-inner');
+//Definir el ancho del contenedor interno "slider-inner"
+ci.css('width', 100*n+'%');
+s.css('width', 100/n+'%');
+
+var next = $('#next');
+var prev = $('#prev');
+
+var i = 0;
+function mover(){
+	if(i===0){
+		ci.css('left',0);
+	} else if (i>0){
+		ci.css('left' , '-' +100*i+'%');
+	}
+	
+}
+next.on('click',function(){
+	if (i<n-1){
+		i++;
+		mover();
+	}
+});
+
+prev.on('click', function(){
+	if (i>0){
+		i--;
+		mover();
+	}
+});
+
+var uno = $('#uno');
+var dos = $('#dos');
+var tres = $('#tres');
+
+uno.on('click',function(){
+	ci.css('left',0);
+	$('#uno').addClass('blue')
+			.addClass('active');
+	$('#dos').removeClass('blue active');
+	$('#tres').removeClass('blue active');
+});
+
+dos.on('click',function(){
+	ci.css('left','-'+100+'%');
+	$('#dos').addClass('blue')
+			.addClass('active');
+	$('#uno').removeClass('blue active');
+	$('#tres').removeClass('blue active');
+});
+
+tres.on('click',function(){
+	ci.css('left','-'+200+'%');
+	$('#tres').addClass('blue')
+			.addClass('active');
+	$('#uno').removeClass('blue active');
+	$('#dos').removeClass('blue active');
+});
+
+//eventos de teclado
+//der 39 izq 37
+//abajo 40 arriba 38
+$(document).on('keydown',function(e){
+	console.log(e.which);
+	if((e.which)==39){
+		next.trigger('click');
+	} else if ((e.which)==37){
+		prev.trigger('click');
+	}
+});
+
+// ******** Evalución ********//
+// var r1;
+var acu;
+var r1 = 0;
+var r2 = 0;
+var r3 = 0;
+var r4 = 0;
+var r5 = 0;
+var text = 0;
+
+$('#op1, #op2, #op3').on('click',function(){
+	var clickedID = this.id;
+
+	if (clickedID === "op3") {
+		r1 = 1;
+	} else{
+		r1 = 0;
+	};
+})
+
+$('#op4, #op5, #op6').on('click',function(){
+	var clickedID = this.id;
+
+	if (clickedID === "op5") {
+		r2 = 1;
+	} else{
+		r2 = 0;
+	};
+})
+
+$('#op7, #op8, #op9').on('click',function(){
+	var clickedID = this.id;
+
+	if (clickedID === "op7") {
+		r3 = 1;
+	} else{
+		r3 = 0;
+	};
+})
+
+$('#op10, #op11, #op12').on('click',function(){
+	var clickedID = this.id;
+
+	if (clickedID === "op11") {
+		r4 = 1;
+	} else{
+		r4 = 0;
+	};
+})
+
+$('#op13, #op14, #op15').on('click',function(){
+	var clickedID = this.id;
+
+	if (clickedID === "op15") {
+		r5 = 1;
+	} else{
+		r5 = 0;
+	};
+})
+
+function evaluar(){
+	//alert(acu);
+	acu = (r1+r2+r3+r4+r5);
+	$("input:radio").attr("checked", false);
+	if (acu >= 4) {
+		tex = "FELICIDADES";
+	} else{
+		//alert("mal");
+		tex = "vuelve a intertar";
+	}
+	$('#Resultado').html('<h6 class="blue-text text-darken-3">TU RESULTADO ES</h6><h1 class="orange-text">'+ acu +'/5</h1><h5 class="blue-text text-darken-3">' + tex + '</h5>');
+}
+
+function cargaSendMail(){
+
+$("#c_enviar").attr("disabled", true);
+
+$(".c_error").remove();
+
+var filter=/^[A-Za-z][A-Za-z0-9_]*@[A-Za-z0-9_]+.[A-Za-z0-9_.]+[A-za-z]$/;
+var s_name = $('#c_name').val();
+var s_email = $('#c_mail').val();
+var s_msg = $('#c_msg').val();
+
+if (filter.test(s_email)){
+sendMail = "true";
+} else{
+$('#c_mail').after("");
+//aplicamos color de borde si el se encontro algun error en el envio
+$('#contactform').css("border-color","#e74c3c");
+sendMail = "false";
+}
+if (s_name.length == 0 ){
+$('#c_name').after( "" );
+var sendMail = "false";
+}
+if (s_msg.length == 0 ){
+$('#c_msg').after( "" );
+var sendMail = "false";
+}
+
+if(sendMail == "true"){
+
+var datos = {
+
+"nombre" : $('#c_name').val(),
+
+"email" : $('#c_mail').val(),
+
+"mensaje" : $('#c_msg').val()
+
+};
+Materialize.toast('Gracias por escribirnos, estamos en contacto.', 4000);
+$.ajax({
+
+data: datos,
+// hacemos referencia al archivo contacto.php
+url: 'php/contacto.php',
+
+type: 'post',
+
+beforeSend: function () {
+//aplicamos color de borde si el envio es exitoso
+$('#contactform').css("border-color","#25A25A");
+
+$("#c_enviar").val("Enviando…");
+
+},
+
+success: function (response) {
+
+$('form')[0].reset();
+$("#c_enviar").val("Enviar");
+$("#c_information p").html(response);
+$("#c_information").fadeIn('slow');
+$("#c_enviar").removeAttr("disabled");
+
+}
+
+});
+
+} else{
+$("#c_enviar").removeAttr("disabled");
+}
+
+}
+function borrar(){
+    document.getElementById("c_name").value = "";
+    document.getElementById("c_mail").value = "";
+    document.getElementById("c_msg").value = "";
+}
